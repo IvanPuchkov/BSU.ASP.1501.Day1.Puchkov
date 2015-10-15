d53e4a1f8f833c257bbebffecd3e67bf9db4b3d1
IvanPuchkov <ivan.puchkov1995@gmail.com>
2015-10-15 22:54:02 +0300

    Added Task1 and Task2

---
+++ NewtonRoot.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class NewtonRoot
+{
+    public static double Root(double c, int n, double eps)
+    {
+        if (n <= 0)
+            throw new ArgumentException();
+        if ((c < 0) && (n % 2 == 0))
+        {
+            return double.NaN;
+        }
+
+        double prevValue;
+        double curValue = 1;
+        double pow;
+        int iteration = 0;
+        do
+        {
+            prevValue = curValue;
+            iteration++;
+            pow = 1;
+            for (int i = 0; i < n - 1; i++)
+                pow *= prevValue;
+
+
+            curValue = (prevValue * (n - 1) + c / pow) / n;
+        }
+        while (Math.Abs(curValue - prevValue) > eps);
+
+        return curValue;
+    }
+}
